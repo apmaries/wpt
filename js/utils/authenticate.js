dfe3b1dd2e11fa3852aa437c14aa02cbf65ce39a
Andy Maries <andy.maries@genesys.com>
2024-02-23 15:59:31 +1100

    Remove unnecessary code and move authentication script to a separate file

---
+++ js/utils/authenticate.js
@@ -0,0 +1,37 @@
+let apiClient;
+
+function authenticate() {
+  // Assuming your hypothetical client library has a loginImplicitGrant method
+  //apiClient = new ExampleAPIClient(); // Replace with the actual SDK's constructor
+  //apiClient.loginImplicitGrant(clientId, redirectUri);
+
+  //clear session storage
+  sessionStorage.clear();
+  console.log("WPT: WEM Power Tools initiated");
+
+  // Set Genesys Cloud objects
+  const platformClient = require("platformClient");
+  const apiClient = platformClient.ApiClient.instance;
+
+  const clientId = document.getElementById("clientIdInput").value;
+  const environment = document.getElementById("clientRegionInput").value;
+  const redirectUri = "https://apmaries.github.io/wpt/wpt.html";
+
+  console.log("WPT: Setting client id and environment session storage items");
+
+  sessionStorage.setItem("clientId", clientId);
+  sessionStorage.setItem("environment", environment);
+
+  apiClient.setEnvironment(environment);
+  apiClient.setPersistSettings(true, "_wpt_");
+
+  let state;
+  try {
+    console.log("WPT: Logging in to GC");
+    apiClient.loginImplicitGrant(clientId, redirectUri, {
+      state: state,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}

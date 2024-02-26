94cb09df119d62e6032295820f487ab5a9bed410
Andy Maries <andy.maries@genesys.com>
2024-02-26 13:25:07 +1100

    Refactor session.js and wpt.html

--- js/utils/session.js
+++ js/utils/session.js
@@ -23,67 +23,65 @@ sessionStorage.setItem("access_token", accessToken);
 sessionStorage.setItem("expires_in", expiresIn);
 sessionStorage.setItem("token_type", tokenType);
 
-// Set up the client
-var platformClient = window.require("platformClient");
-var client = platformClient.ApiClient.instance;
-client.setAccessToken(accessToken);
+// Only make API calls if accessToken has a value
+if (accessToken) {
+  // Set up the client
+  var platformClient = window.require("platformClient");
+  var client = platformClient.ApiClient.instance;
+  client.setAccessToken(accessToken);
 
-// Create API instances
-var usersApi = new platformClient.UsersApi();
-var organizationsApi = new platformClient.OrganizationApi();
-var oAuthApi = new platformClient.OAuthApi();
+  // Create API instances
+  var usersApi = new platformClient.UsersApi();
+  var organizationsApi = new platformClient.OrganizationApi();
+  var oAuthApi = new platformClient.OAuthApi();
 
-// Define the index page
-const indexPage = "https://apmaries.github.io/wpt/index.html";
-
-// Function to check if internal user
-function internalUserCheck(emailAddress) {
-  const domain = emailAddress.split("@")[1];
-  if (domain.toLowerCase() === "genesys.com") {
-    console.log("WPT: Authorised user");
-  } else {
-    console.log("WPT: Unauthorised user!");
-    alert("Sorry, you are not authorised to use this page :(");
-    sessionStorage.clear();
-    window.location.replace(indexPage);
+  // Function to check if internal user
+  function internalUserCheck(emailAddress) {
+    const domain = emailAddress.split("@")[1];
+    if (domain.toLowerCase() === "genesys.com") {
+      console.log("WPT: Authorised user");
+    } else {
+      console.log("WPT: Unauthorised user!");
+      alert("Sorry, you are not authorised to use this page :(");
+      sessionStorage.clear();
+      window.location.replace("https://apmaries.github.io/wpt/index.html");
+    }
   }
+
+  // Get the logged-in user
+  usersApi
+    .getUsersMe()
+    .then(function (user) {
+      // Store the user's name in sessionStorage
+      sessionStorage.setItem("user_name", user.name);
+      sessionStorage.setItem("user_id", user.id);
+
+      // Check if internal user
+      internalUserCheck(user.email);
+
+      // Get the client id from session storage
+      var clientId = sessionStorage.getItem("client_id");
+
+      // Run the getOrganizationsMe() and getOauthClient() calls in parallel
+      return Promise.all([
+        organizationsApi.getOrganizationsMe(),
+        oAuthApi.getOauthClient(clientId),
+      ]);
+    })
+    .then(function (results) {
+      // The results array contains the results of the two API calls
+      var org = results[0];
+      var client = results[1];
+
+      // Store the org name & id in sessionStorage
+      sessionStorage.setItem("org_name", org.name);
+      sessionStorage.setItem("org_id", org.id);
+
+      // Store the client name and scope in sessionStorage
+      sessionStorage.setItem("client_name", client.name);
+      sessionStorage.setItem("client_scope", client.scope);
+    })
+    .catch(function (error) {
+      console.error("WPT: Error: ", error);
+    });
 }
-
-// Get the logged-in user
-usersApi
-  .getUsersMe()
-  .then(function (user) {
-    // Store the user's name in sessionStorage
-    sessionStorage.setItem("user_name", user.name);
-    sessionStorage.setItem("user_id", user.id);
-
-    // Check if internal user
-    internalUserCheck(user.email);
-  })
-  .catch(function (error) {
-    console.error("WPT: Error getting user: ", error);
-  });
-
-// Get the org details
-organizationsApi
-  .getOrganizationsMe()
-  .then(function (org) {
-    // Store the org name & id in sessionStorage
-    sessionStorage.setItem("org_name", org.name);
-    sessionStorage.setItem("org_id", org.id);
-  })
-  .catch(function (error) {
-    console.error("WPT: Error getting organization: ", error);
-  });
-
-// Get the oauth client scope
-oAuthApi
-  .getOauthClient("wpt")
-  .then(function (client) {
-    // Store the client name and scope in sessionStorage
-    sessionStorage.setItem("client_name", client.name);
-    sessionStorage.setItem("client_scope", client.scope);
-  })
-  .catch(function (error) {
-    console.error("WPT: Error getting client scope: ", error);
-  });

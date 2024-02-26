4e199bb2ad7aebf7e0ea73aa51744c7f2a6688be
Andy Maries <andy.maries@genesys.com>
2024-02-26 15:31:10 +1100

    Fix authentication logic and redirect to login page if no token found

--- js/components/subheader.js
+++ js/components/subheader.js
@@ -1,11 +1,11 @@
 window.onload = function () {
   const token = sessionStorage.getItem("token");
   if (!token) {
+    alert(`No token found! Redirecting to login page.`);
     sessionStorage.clear();
-    alert(`Not authenticated. Redirecting to login page.`);
-    //window.location.href = "https://apmaries.github.io/wpt/index.html";
+    window.location.href = "https://apmaries.github.io/wpt/index.html";
   }
   const authText = document.getElementById("authenticatedSubHeader");
-  const oName = sessionStorage.getItem("orgName");
+  const oName = sessionStorage.getItem("org_name");
   authText.innerHTML = `Authenticated in: ${oName}`;
 };

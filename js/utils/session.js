c514ad0d6ad72b803949297a92dc470f2618ba9d
Andy Maries <andy.maries@genesys.com>
2024-02-26 12:50:44 +1100

    Refactor authentication code and fix console logs

--- js/utils/session.js
+++ js/utils/session.js
@@ -1,18 +1,39 @@
-// Declare global variables
-//const indexPage = "https://apmaries.github.io/wpt/index.html";
+// Extract the fragment identifier
+var hash = window.location.hash;
 
-// Functions start here
-function getParameterByName(name) {
-  name = name.replace(/[\\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\#&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.hash);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+// Remove the '#' symbol
+var tokenString = hash.substring(1);
 
-// check if account being used to log in with is internal genesys
-/*function internalUserCheck(emailAddress) {
+// Create a URLSearchParams object
+var urlParams = new URLSearchParams(tokenString);
+
+// Extract the access token
+var accessToken = urlParams.get("access_token");
+
+// Extract other parameters
+var expiresIn = urlParams.get("expires_in");
+var tokenType = urlParams.get("token_type");
+
+// Store the parameters in sessionStorage
+sessionStorage.setItem("access_token", accessToken);
+sessionStorage.setItem("expires_in", expiresIn);
+sessionStorage.setItem("token_type", tokenType);
+
+// Set up the client
+var platformClient = window.require("platformClient");
+var client = platformClient.ApiClient.instance;
+client.setAccessToken(accessToken);
+
+// Create API instances
+var usersApi = new platformClient.UsersApi();
+var organizationsApi = new platformClient.OrganizationApi();
+var oAuthApi = new platformClient.OAuthApi();
+
+// Define the index page
+const indexPage = "https://apmaries.github.io/wpt/index.html";
+
+// Function to check if internal user
+function internalUserCheck(emailAddress) {
   const domain = emailAddress.split("@")[1];
   if (domain.toLowerCase() === "genesys.com") {
     console.log("WPT: Authorised user");
@@ -21,157 +42,40 @@ function getParameterByName(name) {
     alert("Sorry, you are not authorised to use this page :(");
     window.location.replace(indexPage);
   }
-}*/
-
-// get user details
-async function getUser() {
-  try {
-    let udata = await makeApiCallWithRetry(`/api/v2/users/me`, "GET");
-    if (udata) {
-      console.log("WPT: User details returned", udata);
-      const userName = udata.name;
-      const userId = udata.id;
-      const userEmail = udata.email;
-      internalUserCheck(userEmail);
-      sessionStorage.setItem("userName", userName);
-      sessionStorage.setItem("userId", userId);
-    } else {
-      console.error(`WPT: Error getting user details. `, udata);
-      window.location.replace(indexPage);
-    }
-  } catch (error) {
-    console.error(`WPT: Error getting user`, error);
-    window.location.replace(indexPage);
-  }
 }
 
-// get remaining org deets and open notifications channel
-async function getOrgLevelStuff() {
-  // make sure user is authorised before returning more sensitive data
-  await getUser();
+// Get the logged-in user
+usersApi
+  .getUsersMe()
+  .then(function (user) {
+    // Store the user's name in sessionStorage
+    sessionStorage.setItem("user_name", user.name);
+    sessionStorage.setItem("user_id", user.id);
+  })
+  .catch(function (error) {
+    console.error("WPT: Error getting user: ", error);
+  });
 
-  // Wrap each fetch operation in an async function
+// Get the org details
+organizationsApi
+  .getOrganizationsMe()
+  .then(function (org) {
+    // Store the org name & id in sessionStorage
+    sessionStorage.setItem("org_name", org.name);
+    sessionStorage.setItem("org_id", org.id);
+  })
+  .catch(function (error) {
+    console.error("WPT: Error getting organization: ", error);
+  });
 
-  // Fetch the org details
-  let orgPromise = (async () => {
-    let org = await makeApiCallWithRetry(`/api/v2/organizations/me`, "GET");
-    if (org) {
-      console.log("WPT: Org details returned");
-      const orgName = org.name;
-      const orgId = org.id;
-      sessionStorage.setItem("orgName", orgName);
-      sessionStorage.setItem("orgId", orgId);
-    } else {
-      console.error(`WPT: Error getting org details`);
-    }
-  })();
-
-  // Fetch the OAuth client details
-  let clientPromise = (async () => {
-    const clientId = sessionStorage.getItem("clientId");
-    let client = await makeApiCallWithRetry(
-      `/api/v2/oauth/clients/${clientId}`,
-      "GET"
-    );
-    if (client) {
-      console.log("WPT: OAuth client details returned");
-      const clientName = client.name;
-      // TODO: Future consideration to validate client scope prior to execution
-      const clientScope = client.scope;
-      sessionStorage.setItem("clientName", clientName);
-      sessionStorage.setItem("clientScope", clientScope);
-    } else {
-      console.error(`WPT: Error getting OAuth client`);
-    }
-  })();
-
-  // Fetch the divisions
-  let divisionsPromise = (async () => {
-    let divisions = await makeApiCallWithRetry(
-      `/api/v2/authorization/divisions?pageSize=1000&pageNumber=1`,
-      "GET"
-    );
-    if (divisions) {
-      console.log("WPT: Divisions data returned");
-      // TODO: Future cosideration to make application division aware (e.g. what BU's are returned)
-      sessionStorage.setItem("divisionsList", JSON.stringify(divisions));
-    } else {
-      console.error(`WPT: Error getting divisions`);
-    }
-  })();
-
-  // Open the notifications channel
-  let channelPromise = (async () => {
-    let channel = await makeApiCallWithRetry(
-      `/api/v2/notifications/channels`,
-      "POST"
-    );
-    if (channel) {
-      console.log("WPT: Notifications channel opened");
-      const notificationsUri = channel.connectUri;
-      const notificationsId = channel.id;
-      sessionStorage.setItem("notificationsUri", notificationsUri);
-      sessionStorage.setItem("notificationsId", notificationsId);
-    } else {
-      console.error(`WPT: Error creating notifications channel`);
-    }
-  })();
-
-  // Run all fetch operations concurrently
-  await Promise.all([
-    orgPromise,
-    clientPromise,
-    divisionsPromise,
-    channelPromise,
-  ]);
-}
-
-// Define the timeout function
-function timeout() {
-  const notificationsId = sessionStorage.getItem("notificationsId");
-
-  try {
-    // delete notifications channel subscriptions
-    x = makeApiCallWithRetry(
-      `/notifications/channels/${notificationsId}/subscriptions`,
-      "DELETE"
-    );
-
-    // delete the token
-    x = makeApiCallWithRetry(`/tokens/me`, "DELETE");
-  } catch (error) {
-    console.error(`WPT: Error disconnecting: ${error}`);
-  }
-
-  // Clear the session storage and redirect to the login page
-  sessionStorage.clear;
-  window.location.replace(indexPage);
-  console.log("WPT: Timeout due to inactivity.");
-}
-
-// Function to reset the activity timer
-function resetActivityTimer() {
-  clearTimeout(activityTimeout);
-  activityTimeout = setTimeout(timeout, 15 * 60 * 1000); // 15 minutes in milliseconds
-}
-// Functions end here
-
-// main code starts here
-let activityTimeout;
-
-if (window.location.hash) {
-  // Set the token in session storage
-  console.log("WPT: Retrieving access token");
-  const token = getParameterByName("access_token");
-  sessionStorage.setItem("token", token);
-
-  // Get the user details and open the notifications channel
-  //getOrgLevelStuff();
-
-  // Start the initial activity timer
-  resetActivityTimer();
-
-  // Add event listeners to detect user activity
-  document.addEventListener("mousemove", resetActivityTimer);
-  document.addEventListener("keydown", resetActivityTimer);
-}
+// Get the oauth client scope
+oAuthApi
+  .getOauthClient("wpt")
+  .then(function (client) {
+    // Store the client name and scope in sessionStorage
+    sessionStorage.setItem("client_name", client.name);
+    sessionStorage.setItem("client_scope", client.scope);
+  })
+  .catch(function (error) {
+    console.error("WPT: Error getting client scope: ", error);
+  });

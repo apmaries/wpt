ede9ef3e13f2b6672070e45ec1122ec6f029f430
Andy Maries <andy.maries@genesys.com>
2024-02-28 10:11:40 +1100

    Update disconnect button event listener

--- js/utils/disconnect.js
+++ js/utils/disconnect.js
@@ -61,7 +61,7 @@ document.addEventListener("keydown", resetActivityTimer);
 // Run disconnect() when id="disconnectButton" is clicked
 document
   .getElementById("disconnectButton")
-  .addEventListener("click", disconnect);
+  .addEventListener("click", disconnectWithConfirmation);
 
 // TODO: Fix this so that navigation doesn't disconnect the user
 // Run disconnect() when user closes the window

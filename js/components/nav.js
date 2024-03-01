3861ded8cc8a881aa5cc77cc15ca5dbb8b00a266
Andy Maries <andy.maries@genesys.com>
2024-03-01 11:17:33 +1100

    Add navigation component

---
+++ js/components/nav.js
@@ -0,0 +1,17 @@
+const navItems = document.getElementById("nav-items");
+const navObjects = [
+  { href: "/pages/gf.html", text: "Gamification" },
+  { href: "/pages/pd.html", text: "People & Directory" },
+  { href: "/pages/qm.html", text: "Quality Management" },
+  { href: "/pages/st.html", text: "Speech & Text Analytics" },
+  { href: "/pages/wm.html", text: "Workforce Management" },
+];
+
+navObjects.forEach((navObject) => {
+  const li = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = navObject.href;
+  link.textContent = navObject.text;
+  li.appendChild(link);
+  navItems.appendChild(li);
+});

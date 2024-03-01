8c8bb6016df3d746c5f4c1b13313ee2c2ac0cfbc
Andy Maries <andy.maries@genesys.com>
2024-03-01 12:13:52 +1100

    Add test_mode

--- js/utils/session.js
+++ js/utils/session.js
@@ -8,77 +8,78 @@ import { disconnect } from "./disconnect.js";
 
 // Description: This file initiates the session and checks if the user is authorised
 
-if (!sessionStorage.getItem("sesion_active")) {
-  console.log("WPT: Session starting...");
-  // Function to check if internal user
-  function internalUserCheck(emailAddress) {
-    const domain = emailAddress.split("@")[1];
-    if (domain.toLowerCase() === "genesys.com") {
-      console.log(`WPT: User ${emailAddress} is authorised for use`);
-    } else {
-      console.error(`WPT: User ${emailAddress} is not authorised for use!`);
-      alert("Sorry, you are not authorised to use this page :(");
-      sessionStorage.clear();
-      window.location.replace("https://apmaries.github.io/wpt/index.html");
+if (!sessionStorage.getItem("test_mode")) {
+  if (!sessionStorage.getItem("sesion_active")) {
+    console.log("WPT: Session starting...");
+    // Function to check if internal user
+    function internalUserCheck(emailAddress) {
+      const domain = emailAddress.split("@")[1];
+      if (domain.toLowerCase() === "genesys.com") {
+        console.log(`WPT: User ${emailAddress} is authorised for use`);
+      } else {
+        console.error(`WPT: User ${emailAddress} is not authorised for use!`);
+        alert("Sorry, you are not authorised to use this page :(");
+        sessionStorage.clear();
+        window.location.replace("https://apmaries.github.io/wpt/index.html");
+      }
     }
-  }
 
-  // Get the logged in user
-  const user = await handleApiCalls("UsersApi.getUsersMe");
-  // check if internal user
-  internalUserCheck(user.email);
+    // Get the logged in user
+    const user = await handleApiCalls("UsersApi.getUsersMe");
+    // check if internal user
+    internalUserCheck(user.email);
 
-  // Set user details in session storage
-  sessionStorage.setItem("user_name", user.name);
-  sessionStorage.setItem("user_id", user.id);
+    // Set user details in session storage
+    sessionStorage.setItem("user_name", user.name);
+    sessionStorage.setItem("user_id", user.id);
 
-  let org;
-  let client;
+    let org;
+    let client;
 
-  try {
-    // Synchronously return session related data
-    [org, client] = await Promise.all([
-      handleApiCalls("OrganizationApi.getOrganizationsMe"),
-      handleApiCalls(
-        "OAuthApi.getOauthClient",
-        sessionStorage.getItem("client_id")
-      ),
+    try {
+      // Synchronously return session related data
+      [org, client] = await Promise.all([
+        handleApiCalls("OrganizationApi.getOrganizationsMe"),
+        handleApiCalls(
+          "OAuthApi.getOauthClient",
+          sessionStorage.getItem("client_id")
+        ),
 
-      // TODO: Possible enhancement for later if all timezones need to be read
-      // handleApiCalls("UtilitiesApi.getTimezones", globalPageOpts),
+        // TODO: Possible enhancement for later if all timezones need to be read
+        // handleApiCalls("UtilitiesApi.getTimezones", globalPageOpts),
 
-      // TODO: Create notifications channel
-    ]);
+        // TODO: Create notifications channel
+      ]);
 
-    // Continue executing dependent code
-    // Update the subheader
-    const authText = document.getElementById("authenticatedSubHeader");
-    authText.innerHTML = `${user.name} authenticated in: ${org.name}`;
+      // Continue executing dependent code
+      // Update the subheader
+      const authText = document.getElementById("authenticatedSubHeader");
+      authText.innerHTML = `${user.name} authenticated in: ${org.name}`;
 
-    // Store the org name & id in sessionStorage
-    sessionStorage.setItem("org_name", org.name);
-    sessionStorage.setItem("org_id", org.id);
+      // Store the org name & id in sessionStorage
+      sessionStorage.setItem("org_name", org.name);
+      sessionStorage.setItem("org_id", org.id);
 
-    // Store the client name and scope in sessionStorage
-    sessionStorage.setItem("client_name", client.name);
-    // TODO: Future enhancement to validate client scope against list of scopes needed for tools and only show tools that have been authorised
-    sessionStorage.setItem("client_scope", client.scope);
+      // Store the client name and scope in sessionStorage
+      sessionStorage.setItem("client_name", client.name);
+      // TODO: Future enhancement to validate client scope against list of scopes needed for tools and only show tools that have been authorised
+      sessionStorage.setItem("client_scope", client.scope);
 
-    // Set the flag in sessionStorage indicating that session is active
-    sessionStorage.setItem("sesion_active", "true");
-    console.log("WPT: Session started successfully!");
-  } catch (error) {
-    console.error(
-      "WPT: Error occurred while fetching session data! User will be disconnected."
-    );
+      // Set the flag in sessionStorage indicating that session is active
+      sessionStorage.setItem("sesion_active", "true");
+      console.log("WPT: Session started successfully!");
+    } catch (error) {
+      console.error(
+        "WPT: Error occurred while fetching session data! User will be disconnected."
+      );
 
-    // Handle the error here
-    //alert("An error occurred while fetching session data. Please try again.");
-    //disconnect();
-  }
+      // Handle the error here
+      //alert("An error occurred while fetching session data. Please try again.");
+      //disconnect();
+    }
 
-  // Testing for pagination and error handling
-  /* forced 400 error seems to work fine
+    // Testing for pagination and error handling
+    /* forced 400 error seems to work fine
   try {
     const forcedError = await handleApiCalls(
       "GamificationApi.postGamificationProfilesUsersMeQuery",
@@ -94,7 +95,7 @@ if (!sessionStorage.getItem("sesion_active")) {
   }
   */
 
-  /* Pagination seems to work fine :)
+    /* Pagination seems to work fine :)
   try {
     const forcedPagination = await handleApiCalls(
       "UtilitiesApi.getTimezones",
@@ -114,13 +115,16 @@ if (!sessionStorage.getItem("sesion_active")) {
     //disconnect();
   }
   */
+  } else {
+    console.debug("WPT: Home page loaded with active session");
+
+    let user = sessionStorage.getItem("user_name");
+    let org = sessionStorage.getItem("org_name");
+
+    // Update the subheader
+    const authText = document.getElementById("authenticatedSubHeader");
+    authText.innerHTML = `${user} authenticated in: ${org}`;
+  }
 } else {
-  console.debug("WPT: Home page loaded with active session");
-
-  let user = sessionStorage.getItem("user_name");
-  let org = sessionStorage.getItem("org_name");
-
-  // Update the subheader
-  const authText = document.getElementById("authenticatedSubHeader");
-  authText.innerHTML = `${user} authenticated in: ${org}`;
+  console.debug("WPT: Session started with test mode");
 }

4c11377a17155c17f35f171a24af891297682573
Andy Maries <andy.maries@genesys.com>
2024-03-01 11:23:29 +1100

    Add dynamic nav items propegation in nav.js

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,17 +1,21 @@
-const navItems = document.getElementById("nav-items");
-const navObjects = [
-  { href: "/pages/gf.html", text: "Gamification" },
-  { href: "/pages/pd.html", text: "People & Directory" },
-  { href: "/pages/qm.html", text: "Quality Management" },
-  { href: "/pages/st.html", text: "Speech & Text Analytics" },
-  { href: "/pages/wm.html", text: "Workforce Management" },
-];
+document.addEventListener("DOMContentLoaded", () => {
+  const navItems = document.getElementById("nav-items");
+  console.debug("navItems", navItems);
+  const navObjects = [
+    { href: "/pages/gf.html", text: "Gamification" },
+    { href: "/pages/pd.html", text: "People & Directory" },
+    { href: "/pages/qm.html", text: "Quality Management" },
+    { href: "/pages/st.html", text: "Speech & Text Analytics" },
+    { href: "/pages/wm.html", text: "Workforce Management" },
+  ];
 
-navObjects.forEach((navObject) => {
-  const li = document.createElement("li");
-  const link = document.createElement("a");
-  link.href = navObject.href;
-  link.textContent = navObject.text;
-  li.appendChild(link);
-  navItems.appendChild(li);
+  navObjects.forEach((navObject) => {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = navObject.href;
+    link.textContent = navObject.text;
+    li.appendChild(link);
+    navItems.appendChild(li);
+    console.debug(`Added ${navObject.text} to nav`);
+  });
 });

9d12a4a75321a0fb50e17f05946ffbb032183cc4
Andy Maries <andy.maries@genesys.com>
2024-03-21 15:23:53 +1100

    updates

--- js/utils/dropdownHandler.js
+++ js/utils/dropdownHandler.js
@@ -9,17 +9,61 @@ export async function populateDropdown(dropdown, data) {
     return;
   }
 
-  // sort data by name (not case sensitive)
-  data.sort((a, b) =>
-    a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
-  );
+  if (typeof data[0] === "object") {
+    // sort data by name (not case sensitive)
+    data.sort((a, b) =>
+      a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
+    );
+    dropdown.innerHTML = "";
+    data.forEach((item) => {
+      const option = document.createElement("gux-option");
+      option.value = item.id;
+      option.name = item.name;
+      option.innerHTML = item.name;
+      dropdown.appendChild(option);
+    });
+  } else if (typeof data[0] === "string") {
+    // sort data
+    data.sort();
+    dropdown.innerHTML = "";
+    data.forEach((item) => {
+      const option = document.createElement("gux-option");
+      option.value = item;
+      option.innerHTML = item;
+      dropdown.appendChild(option);
+    });
+  }
+}
 
-  dropdown.innerHTML = "";
-  data.forEach((item) => {
-    const option = document.createElement("gux-option");
-    option.value = item.id;
-    option.name = item.name;
-    option.innerHTML = item.name;
-    dropdown.appendChild(option);
-  });
+export async function populateMultiDropdown(dropdown, data) {
+  if (data.length === 0) {
+    dropdown.innerHTML =
+      '<gux-option-multi value="">No data found</gux-option-multi>';
+    return;
+  }
+
+  if (typeof data[0] === "object") {
+    // sort data by name (not case sensitive)
+    data.sort((a, b) =>
+      a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
+    );
+    dropdown.innerHTML = "";
+    data.forEach((item) => {
+      const option = document.createElement("gux-option-multi");
+      option.value = item.id;
+      option.name = item.name;
+      option.innerHTML = item.name;
+      dropdown.appendChild(option);
+    });
+  } else if (typeof data[0] === "string") {
+    // sort data
+    data.sort();
+    dropdown.innerHTML = "";
+    data.forEach((item) => {
+      const option = document.createElement("gux-option-multi");
+      option.value = item;
+      option.innerHTML = item;
+      dropdown.appendChild(option);
+    });
+  }
 }
